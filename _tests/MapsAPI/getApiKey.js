fbd4b3492813f7cfd6428c7f54a5f679f32d4f2f
Florin Asavei <asaveiflorin@gmail.com>
2018-09-22 15:14:29 +0300

    sync

--- _tests/MapsAPI/getApiKey.js
+++ _tests/MapsAPI/getApiKey.js
@@ -2,4 +2,13 @@
     $.getJSON("apiKey.json", function(json) {
    MAPS_CONFIG = json;
    console.log("MAPS_CONFIG",MAPS_CONFIG); 
+
+(function insertAPIKeycript(){
+    var generateScripTag = '<script src="https://maps.googleapis.com/maps/api/js?key=' + MAPS_CONFIG.APIKey + '"&callback=' + "initMapCallback" +'"async defer></script>';
+    $("#APIScript").html(generateScripTag);  
+    debugger;  
+})();
+
 });})()
+
+
